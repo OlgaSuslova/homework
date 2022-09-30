9703fb59ea1a44cf22bac76db0328c4585f708e8
olgasuslova <alekssportmen@gmail.com>
2022-09-30 23:57:11 +0300

    add Task 01

---
+++ Task01/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.WriteLine ("Введите первое число: ");
+string userInput1 = Console.ReadLine() ?? "";
+int userNumber1 = int.Parse(userInput1);
+
+Console.WriteLine ("Введите второе число: ");
+string userInput2 = Console.ReadLine() ?? "";
+int userNumber2 = int.Parse(userInput2);
+
+
+if (userNumber1 == userNumber2)
+{
+    Console.WriteLine ("Числа равны");
+}
+
+else if (userNumber1>userNumber2)
+{
+    Console.WriteLine (userNumber1 + " -> max");
+    Console.WriteLine (userNumber2 + " -> min");
+}
+else if (userNumber1<userNumber2)
+{
+    Console.WriteLine (userNumber1 + " -> min");
+    Console.WriteLine (userNumber2 + " -> max");
+}
