d6e58e9e445f3e1907166f946f35a35aab8b0b38
olgasuslova <alekssportmen@gmail.com>
2022-10-01 22:44:26 +0300

    Added Task 04

---
+++ Task04/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine ("Введите число: ");
+string userInput = Console.ReadLine() ?? "";
+int userNumber = int.Parse(userInput);
+
+int count = 2;
+
+while (count<=userNumber)
+{
+    Console.Write (count + " ");
+    count = count + 2;
+}
