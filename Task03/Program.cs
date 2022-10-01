aa464205e46519ff6e344114e765d5d6576a3301
olgasuslova <alekssportmen@gmail.com>
2022-10-01 21:48:55 +0300

    Added Task 03

---
+++ Task03/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine ("Введите число: ");
+string userInput = Console.ReadLine() ?? "";
+int userNumber = int.Parse(userInput);
+
+if (userNumber%2 == 0)
+{
+    Console.WriteLine ("Число чётное");
+}
+else
+{
+    Console.WriteLine ("Число нечётное ");
+}
